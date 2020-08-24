27bda7b427b49148d48fa636e9edd82b0aadc9e5
Oliver Stapel <hardy.ecc95@gmail.com>
2020-08-24 20:19:03 +0200

    Trying to get pypi description right again

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     description="Roles of persons, their particulars",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=long_description + '\n\n' + history,
     long_description_content_type="text/markdown",
     include_package_data=True,
     keywords='person, roles',
